f0aa1fe2b223d1221abd6ae63ff0968281c333ab
kate <kate@dariox.club>
2024-01-21 21:00:41 +0800

    Set title based off first h1

--- web/pages/guide.php
+++ web/pages/guide.php
@@ -4,9 +4,14 @@ if (isset($_REQUEST['i']))
 {
     $guideId = basename($_REQUEST['i']);
     $content = retriveGuideContent($guideId);
+    
     if ($content == null) {
         header('Location: /guide/index');
     } else {
+        $r = getTextBetweenTags(strval($content), "h1");
+        if (count($r) > 0) {
+            $smarty->assign('title', $r[0] . ' - Xenia Bot Guide');
+        }
         $smarty->assign('guideContent', $content);
         $smarty->assign('guideId', $guideId);
     }
