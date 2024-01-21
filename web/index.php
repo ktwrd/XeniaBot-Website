f33b82775ac7693f57f666aa82c4c7dae7adae8e
kate <kate@dariox.club>
2024-01-21 21:00:25 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,6 +1,9 @@
 <?php
 require 'vendor/autoload.php';
 
+// ini_set('display_errors', '1');
+// ini_set('display_startup_errors', '1');
+// error_reporting(E_ALL);
 
 $time = explode(' ', microtime());
 $begintime = $time[1] + $time[0];
