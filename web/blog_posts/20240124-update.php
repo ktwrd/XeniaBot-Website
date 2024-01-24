4e808b4740ec2c22ee35255e6b464eed57829bb9
kate <kate@dariox.club>
2024-01-24 19:56:38 +0800

    Add new blog post

---
+++ web/blog_posts/20240124-update.php
@@ -0,0 +1,11 @@
+<?php
+
+$generate_blog_post = array(
+    'subject' => 'Minor Bug Fixes',
+    'description' => '',
+    'created_at' => mktime(19, 36, 38, 01, 24, 2024),
+    'hide_state' => 0,
+    'tags' => array()
+);
+
+?>
