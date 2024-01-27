1cab05391d28eca61eb5c0b2271e1cc43dec5eba
kate <kate@dariox.club>
2024-01-27 13:40:30 +0800

    Add new blog post

---
+++ web/blog_posts/20240127-update.php
@@ -0,0 +1,11 @@
+<?php
+
+$generate_blog_post = array(
+    'subject' => 'Dashboard QoL changes',
+    'description' => 'QoL changes to BanSync on the Dashboard, some changes to guides on the website, and minor bug fixes.',
+    'created_at' => mktime(13, 28, 10, 01, 27, 2024),
+    'hide_state' => 0,
+    'tags' => array('Discord', 'Updates', 'Bot', 'Dashboard', 'Website')
+);
+
+?>
