2ba04ddfac8d16e7a295cba67ac5f96497fe2524
kate <kate@dariox.club>
2024-01-27 13:40:46 +0800

    Update 20240124-update.php

--- web/blog_posts/20240124-update.php
+++ web/blog_posts/20240124-update.php
@@ -2,10 +2,10 @@
 
 $generate_blog_post = array(
     'subject' => 'Minor Bug Fixes',
-    'description' => '',
+    'description' => 'Just routine bug squishing for RolePreserve and some other modules.',
     'created_at' => mktime(19, 36, 38, 01, 24, 2024),
     'hide_state' => 0,
-    'tags' => array()
+    'tags' => array('Discord', 'Updates', 'Bot', 'Dashboard')
 );
 
 ?>
