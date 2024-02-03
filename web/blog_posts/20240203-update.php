b7eed9650d0cd61b43882d9d20bb34eec662a074
kate <kate@dariox.club>
2024-02-03 19:58:19 +0800

    update-20240203

---
+++ web/blog_posts/20240203-update.php
@@ -0,0 +1,11 @@
+<?php
+
+$generate_blog_post = array(
+    'subject' => 'Reminders in the Browser',
+    'description' => 'You can now view Reminders and create warns in the Dashboard! Pretty useful I guess',
+    'created_at' => mktime(19, 52, 07, 02, 03, 2024),
+    'hide_state' => 0,
+    'tags' => array('Discord', 'Updates', 'Bot', 'Dashboard', 'Website')
+);
+
+?>
