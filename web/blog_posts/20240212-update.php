1c3db7e25dd50a4acecb979aa9c045037e1614d0
kate <kate@dariox.club>
2024-02-12 19:43:16 +0800

    Add blog post 2024/02/12

---
+++ web/blog_posts/20240212-update.php
@@ -0,0 +1,13 @@
+<?php
+
+$generate_blog_post = array(
+    'subject' => 'Redesigned Stuff',
+    'description' => 'Design changes to the Xenia Homepage and Dashboard',
+    'created_at' => mktime(19, 34, 24, 02, 12, 2024),
+    'hide_state' => 0,
+    'author' => 'Kate Ward',
+    'author_url' => 'https://kate.pet',
+    'tags' => array('Discord', 'Updates', 'Dashboard', 'Website', 'Design')
+);
+
+?>
