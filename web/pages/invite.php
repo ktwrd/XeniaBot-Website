b6bcd69b81d31a0b5e0689f3574d66164b8f7344
kate <kate@dariox.club>
2024-02-25 15:14:22 +0800

    Update invite permissions.

--- web/pages/invite.php
+++ web/pages/invite.php
@@ -1,7 +1,7 @@
 <?php
 
 $userId = '1067393803427790929';
-$permissions = '415471496311';
+$permissions = '415471496439';
 
 $url = 'https://discord.com/oauth2/authorize?client_id='.$userId.'&scope=bot&permissions='.$permissions;
 
