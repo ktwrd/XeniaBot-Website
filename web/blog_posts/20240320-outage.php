9fc4e6296cf14858fb63478ed080db33858f9473
kate <kate@dariox.club>
2024-03-20 14:40:21 +0800

    .

--- web/blog_posts/20240320-outage.php
+++ web/blog_posts/20240320-outage.php
@@ -2,7 +2,7 @@
 
 $generate_blog_post = array(
     'subject' => 'Service Outage (unplanned)',
-    'description' => 'Unplanned Outage affecting Xenia Bot and Dashboard',
+    'description' => 'Unplanned Outage effecting Xenia Bot and Dashboard',
     'created_at' => mktime(14, 14, 17, 03, 20, 2024),
     'hide_state' => 0,
     'author' => 'Kate Ward',
