44ef1172a3b97adfb062756be43a185bad632e40
kate <kate@dariox.club>
2024-03-29 20:08:46 +0800

    Add blog post 20240329-update

---
+++ web/blog_posts/20240329-update.php
@@ -0,0 +1,14 @@
+<?php
+
+$generate_blog_post = array(
+    'subject' => 'Warn System Update',
+    'description' => 'Some lovely QoL additions to the Warn System.',
+    'created_at' => mktime(19, 27, 50, 03, 29, 2024),
+    'hide_state' => 0,
+    'author' => 'Kate Ward',
+    'author_url' => 'https://kate.pet',
+    'author_email' => 'kate@dariox.club',
+    'tags' => array('Discord', 'Updates', 'Dashboard')
+);
+
+?>
