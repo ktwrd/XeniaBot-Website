1efcd39691cb33c9722f4754a25504367d95e2f3
kate <kate@dariox.club>
2024-04-14 14:31:07 +0800

    Add blog post 20240414-update.php

---
+++ web/blog_posts/20240414-update.php
@@ -0,0 +1,14 @@
+<?php
+
+$generate_blog_post = array(
+    'subject' => 'Dashboard Reactivity!',
+    'description' => 'The Dashboard now has some sort of Reactivity',
+    'created_at' => mktime(13, 59, 14, 04, 14, 2024),
+    'hide_state' => 0,
+    'author' => 'Kate Ward',
+    'author_url' => 'https://kate.pet',
+    'author_email' => 'kate@dariox.club',
+    'tags' => array('Discord', 'Updates', 'Dashboard')
+);
+
+?>
