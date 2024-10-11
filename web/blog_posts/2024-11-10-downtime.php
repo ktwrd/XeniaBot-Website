7996505793d499ce4ca529f64146855c6649f213
kate <kate@dariox.club>
2024-10-11 20:56:02 +0800

    Add new blog post

---
+++ web/blog_posts/2024-11-10-downtime.php
@@ -0,0 +1,14 @@
+<?php
+
+$generate_blog_post = array(
+    'subject' => 'Downtime Resolved',
+    'description' => 'Cryptobros ruin the day again...',
+    'created_at' => mktime(20, 47, 07, 10, 11, 2024),
+    'hide_state' => 0,
+    'author' => 'Kate Ward',
+    'author_url' => 'https://kate.pet',
+    'author_email' => 'kate@dariox.club',
+    'tags' => array('Discord', 'Updates', 'Bot', 'Dashboard')
+);
+
+?>
