36ed5e1b94d0f76a1cc084d2a5b35fccf760f3c6
kate <kate@dariox.club>
2025-11-02 13:31:15 +0800

    Created blog post 20251102-update

---
+++ web/blog_posts/20251102-update.php
@@ -0,0 +1,14 @@
+<?php
+
+$generate_blog_post = array(
+    'subject' => 'Release v1.15',
+    'description' => '',
+    'created_at' => mktime(11, 17, 46, 11, 02, 2025),
+    'hide_state' => 0,
+    'author' => 'Kate Ward',
+    'author_url' => 'https://kate.pet',
+    'author_email' => 'kate@dariox.club',
+    'tags' => array('Discord', 'Updates', 'Dashboard', 'Bot')
+);
+
+?>
