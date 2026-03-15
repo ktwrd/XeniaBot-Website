c84237c46e8b5d9dcc87b9ce66cafd7e1c7e05fb
kate <kate@dariox.club>
2026-03-15 13:32:04 +0800

    Created blog post: 20260315-update

---
+++ web/blog_posts/20260315-update.php
@@ -0,0 +1,14 @@
+<?php
+
+$generate_blog_post = array(
+    'subject' => 'Release v1.16',
+    'description' => 'First update for the new year!',
+    'created_at' => mktime(13, 19, 9, 03, 15, 2026),
+    'hide_state' => 0,
+    'author' => 'Kate Ward',
+    'author_url' => 'https://kate.pet',
+    'author_email' => 'kate@dariox.club',
+    'tags' => array('Discord', 'Updates', 'Dashboard', 'Bot')
+);
+
+?>
